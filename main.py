c7cc1475f63886b7a9f252e5c3da51f0b4ab47c3
ixelizm <57567251+ixelizm@users.noreply.github.com>
2022-03-14 19:48:09 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+@app.route("/recieve")
+def recieve():
+  return "Recieve Router"
+
+@app.route("/send")
+def send():
+  return "send Router"
+
+
+if __name__ == "__main__":
+  app.run(port = 5000)
