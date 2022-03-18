0a53910cae9dc2cc711a309814b47e2ff8a2cb4e
ixelizm <57567251+ixelizm@users.noreply.github.com>
2022-03-19 02:25:49 +0300

    Update nimiq.py

--- nimiq.py
+++ nimiq.py
@@ -20,20 +20,20 @@ r = driver.get(url)
 time.sleep(3)
 lastest = "2050137"
 total = 0
-
-with bot:
-  bot.send_message(-1001721126047, "__Bot Aktif!!__")
-while True:
-  try:
-    lastess = driver.find_element_by_xpath('//*[@id="blocklist"]/div[1]')
-    if str(lastess.text.split("#")[1].split(" ")[0]) != lastest:
-      if lastess.text.split("found by ")[1].split(" ")[0] == "ACEMINING":
-        ids = str(lastess.text.split("#")[1].split(" ")[0])
-        times = str(lastess.text.split(ids + " ")[1].split(" min")[0])
-        transactions = str(lastess.text.split("min ")[1].split(" transactions")[0])
-        size = str(lastess.text.split(" KB")[0].split(" ")[1])
-        with bot:
-          msg = f"""
+def main():
+  with bot:
+    bot.send_message(-1001721126047, "__Bot Aktif!!__")
+  while True:
+    try:
+      lastess = driver.find_element_by_xpath('//*[@id="blocklist"]/div[1]')
+      if str(lastess.text.split("#")[1].split(" ")[0]) != lastest:
+        if lastess.text.split("found by ")[1].split(" ")[0] == "ACEMINING":
+          ids = str(lastess.text.split("#")[1].split(" ")[0])
+          times = str(lastess.text.split(ids + " ")[1].split(" min")[0])
+          transactions = str(lastess.text.split("min ")[1].split(" transactions")[0])
+          size = str(lastess.text.split(" KB")[0].split(" ")[1])
+          with bot:
+            msg = f"""
 **Blok Bulundu!!**
   **ACEMINING.CO**
       **ID:** #{ids}
@@ -41,15 +41,15 @@ while True:
       **Transactions:** {transactions} Transaction
       **Size:** {size} KB
 """
-          bot.send_message(-1001721126047, msg)
-          total += 1
-        lastest = lastess.text.split("#")[1].split(" ")[0]
-  except:
-    pass
+            bot.send_message(-1001721126047, msg)
+            total += 1
+          lastest = lastess.text.split("#")[1].split(" ")[0]
+    except:
+      pass
 def count():
   while True:
     with bot:
       bot.send_message(-1001721126047, f"**1 Saatte Bulunan Blok Sayısı: {total}")
       time.sleep(2)
-      
-t1 = Thread(target=count()).start()
+t1 = Thread(target=main()).start()
+t2 = Thread(target=count()).start()
